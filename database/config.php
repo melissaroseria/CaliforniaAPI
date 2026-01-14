7aea251adf68f95f66ea93f9a8a45ae8bf0fe63d
Melissa <hellofucking@icloud.com>
2026-01-15 02:37:02 +0300

    Create config.php

---
+++ database/config.php
@@ -0,0 +1,17 @@
+<?php
+// config.php
+return [
+    'db' => [
+        'host' => 'localhost',
+        'port' => '3306',
+        'username' => 'localhost',
+        'password' => 'localhost',
+        'dbname' => 'localhost'
+    ],
+    'forum' => [
+        'name' => 'Forum California',
+        'url' => 'https://viosrio.serv00.net/forum/',
+        'year' => 2015 // Nostaljik yıl
+    ]
+];
+?>
